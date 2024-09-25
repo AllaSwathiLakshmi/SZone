cdd76fc22a73d4502926dfe8c93104d28bdda2a7
swathi lakshmi <swathilakshmialla@gmail.com>
2024-09-25 16:33:34 +0530

    swathi-Addednetworking componet-250924

---
+++ src/app/networking/networking.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-networking',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './networking.component.html',
+  styleUrl: './networking.component.css'
+})
+export class NetworkingComponent {
+
+}
