34fc2d4f165d9c02d4354c6178231c3d1ee43d23
KalakondaNihirika <nihirikakalakonda2000@gmail.com>
2025-01-22 15:57:01 +0530

    changes by niharika

--- src/app/solarenergy/solarenergy.component.ts
+++ src/app/solarenergy/solarenergy.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-solarenergy',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './solarenergy.component.html',
   styleUrl: './solarenergy.component.css'
 })
