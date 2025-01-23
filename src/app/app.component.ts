2e50ac02f7bf2d38709f89449c622f462ff2b9e9
Alekhya <your_email@example.com>
2025-01-23 15:25:31 +0530

    save all changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { GalleryComponent } from './gallery/gallery.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, GalleryComponent,
-    HeaderComponent, HomeComponent,LoginComponent, FooterComponent, LoginComponent],
+  imports: [CommonModule, RouterOutlet, RouterModule,
+    HeaderComponent, FooterComponent],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
